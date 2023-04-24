f5164575debae66a1c1fe071e11e87a709a6c61a
David Levine <david@axepress.dev>
2023-04-24 14:18:36 +0000

    chore: update composer deps

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit2295e5a77a3e5bf1a19224863508fc5e
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit2295e5a77a3e5bf1a19224863508fc5e::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire2295e5a77a3e5bf1a19224863508fc5e($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit2295e5a77a3e5bf1a19224863508fc5e::$files;
+        $requireFile = static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        };
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            ($requireFile)($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire2295e5a77a3e5bf1a19224863508fc5e($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'axepress/wp-graphql-site-editor',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'de697d5b30eba8dafe5d68a8840632f445d3d8a9',
+        'reference' => '9c8b2928bcb369ef7d206728e7087f5dc45384c1',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'axepress/wp-graphql-site-editor' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'de697d5b30eba8dafe5d68a8840632f445d3d8a9',
+            'reference' => '9c8b2928bcb369ef7d206728e7087f5dc45384c1',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
